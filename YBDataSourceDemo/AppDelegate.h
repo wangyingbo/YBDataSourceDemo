82111accc5f273d7613ba12b738379149eb2bbe0
wangyingbo <wangyingbo528@126.com>
2017-09-25 15:16:12 +0800

    Initial Commit

---
+++ YBDataSourceDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  YBDataSourceDemo
+//
+//  Created by 王迎博 on 2017/9/25.
+//  Copyright © 2017年 王颖博. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ YBDataSourceDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  YBDataSourceDemo
+//
+//  Created by 王迎博 on 2017/9/25.
+//  Copyright © 2017年 王颖博. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
