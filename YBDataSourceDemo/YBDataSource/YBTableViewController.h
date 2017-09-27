6eaba106aafdb15ce1288eeeb7be395682361419
wangyingbo <wangyingbo528@126.com>
2017-09-27 11:50:14 +0800

    增加注释

--- YBDataSourceDemo/YBDataSource/YBTableViewController.h
+++ YBDataSourceDemo/YBDataSource/YBTableViewController.h
@@ -17,7 +17,7 @@
 @property (nonatomic, weak) UITableView * _Nullable tableView;
 @property (nonatomic,strong) YBDataSource * _Nonnull tableViewDS;
 
-#pragma mark - 自定义的cell，只能调willDisplay方法
+#pragma mark - 自定义的cell(一步到位。具体参见demo里YBListVC里的用法)
 - (void)initDatasourceWithCellClass:(_Nullable Class)cellClass withCellIdentifier:(NSString * _Nullable)cellIdentifier;
 
 
